b3d4023cd6c806090a5f7bf0fe92956dbb90fd55
MJ <za@mm.st>
2024-03-30 11:27:31 +1300

    adding data  and showing it

--- www/src/components/HelloWorld.vue
+++ www/src/components/HelloWorld.vue
@@ -1,13 +1,91 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+// import axios from 'axios'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+interface MyData {
+  // Define the shape of your data here
+  mydata: {}
+}
+
+const mydata = ref<MyData | null>(null)
+const endpoint = import.meta.env.VITE_HASURA_PROJECT_ENDPOINT;
+
+function fetchGraphQL(
+  operationsDoc: string,
+  operationName: string,
+  variables: Record<string, any>
+) {
+  return fetch(endpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: operationsDoc,
+      variables,
+      operationName,
+    }),
+  }).then(result => result.json());
+}
+
+const operation:string = `
+  query MyQuery {
+    friend {
+      name
+    }
+  }
+`;
+
+function fetchMyQuery() {
+  return fetchGraphQL(operation, 'MyQuery', {})
+}
+
+fetchMyQuery()
+  .then(({ data, errors }) => {
+    if (errors) {
+      console.error(errors);
+    }
+    mydata.value  = data;
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// const fetchData = async () => {
+//   try {
+//     //'http://localhost:8080/api/rest/friends'
+//     const endpoint = import.meta.env.VITE_HASURA_PROJECT_ENDPOINT;
+//     const response = await axios.post<MyData>(`${endpoint}`,{
+//       headers: {
+//         'x-hasura-admin-secret': import.meta.env.VITE_HASURA_ADMIN_SECRET
+//       },
+//       body: JSON.stringify({
+//         query: `query MyQuery {
+//           friend {
+//             name
+//           }
+//         }`
+//       })
+//     })
+//     data.value = await response.data
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+onMounted(() => {
+  console.log(import.meta.env.VITE_HASURA_GRAPHQL_METADATA_DATABASE_URL);
+  // console.log(process.env.HASURA_GRAPHQL_METADATA_DATABASE_URL)
+  // fetchData()
+  fetchMyQuery()
+})
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ mydata }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -18,17 +96,14 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+     Hello
   </p>
-  <p>
+  <!-- <p>
     Install
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p> -->
 </template>
 
 <style scoped>
